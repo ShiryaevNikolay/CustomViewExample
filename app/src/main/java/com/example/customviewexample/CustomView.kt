d97ffd9c54bda3445647658777c203c4b38d8871
Nikolay Shiryaev <deendeeps@list.ru>
2021-07-31 21:00:58 +0300

    Устанавливает дефолтный цвет

--- app/src/main/java/com/example/customviewexample/CustomView.kt
+++ app/src/main/java/com/example/customviewexample/CustomView.kt
@@ -1,8 +1,10 @@
 package com.example.customviewexample
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.FrameLayout
+import androidx.annotation.ColorInt
 
 class CustomView @JvmOverloads constructor(
     context: Context, attrs:
@@ -13,7 +15,25 @@ class CustomView @JvmOverloads constructor(
         const val DEFAULT_COLOR_RES = R.color.defaultColorCv
     }
 
-    init {
+    @SuppressLint("ResourceAsColor")
+    @ColorInt
+    private var defaultColorRes = DEFAULT_COLOR_RES
 
+    init {
+        obtainAttrs(context, attrs)
+        initViews()
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private fun obtainAttrs(context: Context, attrs: AttributeSet?) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomView)
+
+        defaultColorRes = typedArray.getColor(R.styleable.CustomView_defaultColor, DEFAULT_COLOR_RES)
+
+        typedArray.recycle()
+    }
+
+    private fun initViews() {
+        setBackgroundColor(defaultColorRes)
     }
 }
